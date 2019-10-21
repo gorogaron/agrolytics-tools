19433bd4bf7316916c3e6dc040561c4bf99f36c2
kenderak <joseph.kenderak@gmail.com>
2019-10-21 23:14:17 +0200

    v1.0

--- image_augmentor/image_augmentor.py
+++ image_augmentor/image_augmentor.py
@@ -4,21 +4,21 @@ import argparse
 import cv2
 import os
 import glob
+import shutil
 import Augmentor
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--inputdir', type=str, required=True)
 parser.add_argument('-m', '--maskdir', type=str, required=True)
+parser.add_argument('-s', '--sample', type=int, required=True)
 args = parser.parse_args()
 
 INPUT_PATH = os.path.normpath(os.path.join(os.getcwd(), args.inputdir))
 MASK_PATH = os.path.normpath(os.path.join(os.getcwd(), args.maskdir))
-print(INPUT_PATH)
-print(MASK_PATH)
 
 p = Augmentor.Pipeline(INPUT_PATH)
-
 p.ground_truth(MASK_PATH)
+
 p.skew(0.3,0.2)
 p.zoom(0.3,1.01,1.1)
 p.rotate(0.2, max_left_rotation=10, max_right_rotation=10)
@@ -27,10 +27,25 @@ p.flip_left_right(0.5)
 p.flip_top_bottom(0.25)
 p.random_distortion(0.3, 10, 10, 10)
 p.shear(0.3, 18, 18)
-p.random_contrast(0.3,0.5,1.7)
 p.random_color(0.3,0.5,2.5)
-p.random_brightness(0.3,0.5,1.5)
 p.histogram_equalisation(0.1)
 
+p.sample(args.sample)
 
-p.sample(50)
+## Moving files to another directory and rename them
+i = 0
+for input_file, mask_file in zip(sorted(glob.glob(os.path.join(INPUT_PATH, 'output', 'train_images_original*.png'))),
+                                 sorted(glob.glob(os.path.join(INPUT_PATH, 'output', '_groundtruth_(1)_train_images*.png')))):
+
+    shutil.move(input_file, INPUT_PATH)
+    shutil.move(mask_file, MASK_PATH)
+
+    new_input_filename, new_mask_filename = os.path.basename(input_file), os.path.basename(mask_file)
+    new_input_path, new_mask_path = os.path.join(INPUT_PATH, new_input_filename), os.path.join(MASK_PATH, new_mask_filename)
+
+    os.rename(new_input_path, os.path.join(INPUT_PATH, 'augmented_' + str(i) + '.png'))
+    os.rename(new_mask_path, os.path.join(MASK_PATH, 'augmented_' + str(i) + '.png'))
+    i = i + 1
+
+## Remove the original output directory
+os.rmdir(os.path.join(INPUT_PATH, 'output'))
