84578be717228f57ddf5586d93d7f15c18214cce
Ubuntu <ubuntu@ip-172-31-27-2.us-west-1.compute.internal>
2019-12-28 18:37:20 +0000

    added models repo to default generation path

--- create_dataset/create_dataset.sh
+++ create_dataset/create_dataset.sh
@@ -1,13 +1,13 @@
 IMG_SRC='../../dataset/images'
 MASK_SRC='../../dataset/masks'
 
-TRAIN_IMG_DST='train_images'
-TRAIN_MASK_DST='train_labels'
-VAL_IMG_DST='val_images'
-VAL_MASK_DST='val_labels'
-NUM_OF_AUGMENT=10
+TRAIN_IMG_DST='../../agrolytics-models/vggsegnet/train_images'
+TRAIN_MASK_DST='../../agrolytics-models/vggsegnet/train_labels'
+VAL_IMG_DST='../../agrolytics-models/vggsegnet/val_images'
+VAL_MASK_DST='../../agrolytics-models/vggsegnet/val_labels'
+NUM_OF_AUGMENT=128
 SIZE='(640, 480)'
-PADDING=1
+PADDING=0
 
 python create_dataset.py -ti=$TRAIN_IMG_DST -tm=$TRAIN_MASK_DST -vi=$VAL_IMG_DST -vm=$VAL_MASK_DST -s="$SIZE" -p=$PADDING
 python ./../albumentations/albumentation.py -i=$IMG_SRC -m=$MASK_SRC -id=$TRAIN_IMG_DST -md=$TRAIN_MASK_DST -s="$SIZE" -q=$NUM_OF_AUGMENT -p=$PADDING
